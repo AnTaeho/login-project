349cd745b4910634333ed0c39a07384ced42880d
antaeho <an981022@naver.com>
2023-07-18 21:00:30 +0900

    feat: 쿠키 기반 로그인 구현
    다음엔 세션 기반으로 변경 예정

---
+++ src/main/java/com/example/loginproject/domain/User.java
@@ -0,0 +1,26 @@
+package com.example.loginproject.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String loginId;
+    private String password;
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+}
---
+++ src/main/java/com/example/loginproject/domain/UserRole.java
@@ -0,0 +1,5 @@
+package com.example.loginproject.domain;
+
+public enum UserRole {
+    USER, ADMIN
+}
