e3799417812124f53188de05ae492d2608562873
antaeho <an981022@naver.com>
2023-07-19 08:43:57 +0900

    feat: spring security 기반 로그인 구현

---
+++ src/main/java/com/example/loginproject/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.example.loginproject.config;
+
+import com.example.loginproject.domain.UserRole;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests( auth -> auth
+                        .requestMatchers("/security-login/info").authenticated()
+                        .requestMatchers("/security-login/admin/**").hasRole(UserRole.ADMIN.name())
+                        .anyRequest().permitAll()
+                )
+                .formLogin( login -> login
+                        .usernameParameter("loginId")
+                        .passwordParameter("password")
+                        .loginPage("/security-login/login")
+                        .defaultSuccessUrl("/security-login")
+                        .failureUrl("/security-login/login")
+                )
+                .logout( logout -> logout
+                        .logoutUrl("/security-login/logout")
+                        .invalidateHttpSession(true).deleteCookies("JSESSIONID")
+                )
+                .build();
+    }
+}
